ff25c95f0de27c52910c9f8ed9b9aae12eb9ecf1
Jaskeerat Singh Randhawa <jaskee789@gmail.com>
2020-05-26 23:57:42 +0530

    pm2 config

---
+++ deploy/deploy.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    deploy: {
+        production: {
+            key: './deploy/Books',
+            user: 'ubuntu',
+            host: ["library.dsctiet.tech"],
+            ref: 'origin/master',
+            repo: 'https://github.com/developer-student-club-thapar/calibre-web',
+            path:'/home/ubuntu/library-production',
+            "post-setup":'pip3 install -r requirements.txt',
+            "post-deploy":"sudo pm2 startOrRestart ./deploy/production.config.js"
+        },
+    }
+}
