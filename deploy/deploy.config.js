cf210ffe1c1c7458015d05ba415e1b92a66bad97
Jaskeerat Singh Randhawa <jaskee789@gmail.com>
2020-06-19 11:52:43 +0530

    disable strict checking

--- deploy/deploy.config.js
+++ deploy/deploy.config.js
@@ -6,7 +6,8 @@ module.exports = {
             host: ["35.213.132.113"],
             ref: 'origin/master',
             repo: 'https://github.com/developer-student-club-thapar/calibre-web',
-            path:'/home/ubuntu/library-production',
+            ssh_options: "StrictHostKeyChecking=no",
+	    path:'/home/ubuntu/library-production',
             "post-setup":'pip3 install -r requirements.txt',
             "post-deploy":"pm2 startOrRestart ./deploy/production.config.js"
         },
